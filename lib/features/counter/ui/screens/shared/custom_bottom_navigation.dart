62a519456466802d87ed1607e0e504de7b7afcc6
hesptech <olea.oswaldo@gmail.com>
2024-10-25 16:19:59 +0200

    feat: BottomNavigationBar badge counter bloc state

--- lib/features/counter/ui/screens/shared/custom_bottom_navigation.dart
+++ lib/features/counter/ui/screens/shared/custom_bottom_navigation.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_akshit/features/counter/ui/screens/bloc_builder_screen.dart';
 import 'package:flutter_bloc_akshit/features/counter/ui/screens/bloc_consumer_screen.dart';
 import 'package:flutter_bloc_akshit/features/counter/ui/screens/bloc_listener_screen.dart';
 
+import '../../../bloc/counter_bloc.dart';
 import '../home_screen.dart';
 
 class CustomBottomNavigation extends StatefulWidget {
@@ -15,10 +17,10 @@ class CustomBottomNavigation extends StatefulWidget {
 class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
   int currentPageIndex = 0;
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-   
       body: <Widget>[
         /// Home page
         const HomeScreen(),
@@ -26,41 +28,48 @@ class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
         const BlocListenerScreen(),
         const BlocConsumerScreen(),
       ][currentPageIndex],
-    
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
+      bottomNavigationBar: BlocBuilder<CounterBloc, CounterState>(  
+        builder: (context, state) {
+
+          //counterBloc.add(CounterInitialevent());
+          final successState = context.watch<CounterBloc>().value;
+
+          return NavigationBar(
+            onDestinationSelected: (int index) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+            indicatorColor: Colors.amber,
+            selectedIndex: currentPageIndex,
+            destinations: <Widget>[
+              const NavigationDestination(
+                selectedIcon: Icon(Icons.home),
+                icon: Icon(Icons.home_outlined),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Badge(
+                  label: Text(successState.toString(),),
+                  child: const Icon(Icons.add_circle_outlined),
+                ),
+                label: 'BLoC Builder',
+              ),
+              const NavigationDestination(
+                icon: Badge(
+                  child: Icon(Icons.hearing_outlined),
+                ),
+                label: 'BLoC Listener',
+              ),
+              const NavigationDestination(
+                icon: Badge(
+                  child: Icon(Icons.library_add_outlined),
+                ),
+                label: 'BLoC Consumer',
+              ),
+            ],
+          );
         },
-        indicatorColor: Colors.amber,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Badge(
-              label: Text('1'),
-              child: Icon(Icons.add_circle_outlined),
-            ),
-            label: 'BLoC Builder',
-          ),
-          NavigationDestination(
-            icon: Badge(
-              child: Icon(Icons.hearing_outlined),
-            ),
-            label: 'BLoC Listener',
-          ),
-          NavigationDestination(
-            icon: Badge(
-              child: Icon(Icons.library_add_outlined),
-            ),
-            label: 'BLoC Consumer',
-          ),
-        ],
       ),
     );
   }
